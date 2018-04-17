533b09bb0bd254cb341c994db7ef5ae7754dde16
Unknown <michelle.groenendijk@hotmail.com>
2018-04-17 12:32:18 +0200

    update opdracht

--- code.js
+++ code.js
@@ -9,3 +9,7 @@ console.log(celsius);
 let fahrenheit = 
     Math.floor(celsius * (9/5)+32);
 console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
+
+let newton = 
+    Math.floor(celsius * (33/100));
+console.log(newton);
